1c58cd06fe77c69d32dbf803cfe9206179eb902d
Alex Lobrano <alexlobrano@gmail.com>
2018-03-27 23:17:37 -0400

    Cleanup of 1b

--- 1b.py
+++ 1b.py
@@ -1,54 +1,25 @@
-# 1a.py
+# 1b.py
 
-# Implement the Miller-Rabin primality testing algorithm presented in class. Given a number N output
-# 1 if it is prime and 0 otherwise.
-# To check your implementation, develop a method to test that a small odd number N is prime. Consider
-# all odd numbers less than p
-# (N) and test if they are divisors of N.
-# Test your Miller-Rabin implementation for 10 small numbers (size n = 20 bits).
+# Using the Miller-Rabin primality testing algorithm, generate two primes p and q of size n = 1024
+# bits, and set N = pq. Implement the RSA trapdoor permutation:
+# - Gen: choose e, d such that e*d = 1 mod phi(N) and output pk = e and sk = d;
+# - Trapdoor(pk, x): given x in Z_N^*, output x^e mod N;
+# - Inverse(sk, y): given y in Z_N^*, output y^d mod N.
+# Generate 10 pairs(x, y) with y = Trapdoor(pk, x) and check correctness: Inverse(sk,Trapdoor(pk, x)) =
+# x.
 
 from Alex_Lobrano_implementation import *
 
-#test for 10 small numbers, size n = 20 bits.
-n = 1024
-
 # test RSA, do it 10 times
-
-# Generate random x in Z_N^*
 
 rsa = RSA()
 rsa.gen()
-x = randnum.randint(1, rsa.rsamodulus - 1)			# generate integer e between 1 and phi(N)-1
-while(fractions.gcd(x, rsa.rsamodulus) != 1):		# check if e is relatively prime to phi(N)
-	x = randnum.randint(1, rsa.rsamodulus - 1)			# if not, generate new e and try again
-	
-# print "X:", x
-# y = rsa.trapdoor(x)
-# print "Y:", y
-# recovered_x = rsa.inverse(y)
-# print "X:", recovered_x
 
-y = rsa.trapdoor(x)
-assert rsa.inverse(y) == x
+for i in range(10):
 
-# p = generate_prime(n)
-# q = generate_prime(n)
-
-# rsa = p*q
-
-# phi = (p-1)*(q-1)
-
-# e = randnum.randint(1, phi-1)			# generate N-1 random bits
-# while(fractions.gcd(e, phi) != 1):
-	# e = randnum.randint(1, phi-1)			# generate N-1 random bits
-	
-# d = modinv(e, phi) + phi
-	
-# print "Phi:", phi
-# print "E:", e
-# print "D:", d
-# print "D + phi:", d+phi
-# print "GCD phi and e:", fractions.gcd(e,phi)
-# print "GCD phi and d:", fractions.gcd(d,phi)
-# print "e*d mod phi:", e*d % phi
-
+	# Generate random x in Z_N^*
+	x = randnum.randint(1, rsa.rsamodulus - 1)			# generate integer x between 1 and N-1
+	while(fractions.gcd(x, rsa.rsamodulus) != 1):		# check if x is relatively prime to N
+		x = randnum.randint(1, rsa.rsamodulus - 1)			# if not, generate new x and try again
+	y = rsa.trapdoor(x)
+	assert rsa.inverse(y) == x
